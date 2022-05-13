5098c68cc2f95f4e28dead3bf30c8b0470a45c96
tupcha <terem.tema2@gmail.com>
2022-05-13 23:04:29 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat);
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1);
+
+        System.out.println(bean==bean1);
+        System.out.println(beanCat==beanCat1);
+    }
+}
